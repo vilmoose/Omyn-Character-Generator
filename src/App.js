847b02588e04ca4d2146d5aa9f2668e38d62c6fa
MariichkaBond <mariiabondar@cmail.carleton.ca>
2025-02-05 21:13:18 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <head>
+        <title>Izotope Games Generator</title>
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=UnifrakturMaguntia" />
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Jacquard 12" />
+      </head>
+      <header className="">
+        
       </header>
+      <body className="App-body">
+        <h1>Izotope Games Character Generator</h1>
+        <a href="https://substack.com/@izotopegames?utm_source=user-menu">Link to Izotop Games</a>
+      </body>
+
     </div>
   );
 }
