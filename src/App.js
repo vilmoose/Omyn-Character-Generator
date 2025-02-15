ae7d7e455ee3f6efe2e90b3c7e5376d1147e1083
vilmoose <vilmosf11@gmail.com>
2025-02-14 23:15:56 -0500

    fixed body issues and minor changes

--- src/App.js
+++ src/App.js
@@ -2,44 +2,40 @@ import './App.css';
 
 function App() {
   return (
+    //<!DOCTYPE html>
+    <html lang="en">
 
-    <div className="App">
-      <head>
-        <title>Izotope Games Generator</title>
-        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=UnifrakturMaguntia" />
-        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Jacquard 12" />
-      </head>
-      <header>
+    <head class="App-head">
+      <title>Izotope Games Generator</title>
+      <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=UnifrakturMaguntia" />
+      <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Jacquard 12" />
+    </head>
 
-      </header>
-      <body className="App-body">
-        <audio autoPlay loop>
-          <source src="/audio/background_audio.wav" type="audio/wav" />
-          Your browser does not support the audio element.
-        </audio>
-        <h1>Izotope Games Character Generator</h1>
-        <div className="Generator-div">
-          <a href="https://substack.com/@izotopegames?utm_source=user-menu">Link to Izotop Games</a>
-          <br/>
-          <button>Generate</button>
-          <button>Help</button>
-          <p>
-            This is where the generator button and help button will be.
-          </p>
-        </div>
-        <div className="List-div">
-          <p>
-            This is where generated characters will go in separate boxes.
-            Will need to implement a list.
-          </p>
-          <ol>
-            <li>Character 1</li>
-            <li>Character 2</li>
-          </ol>
-        </div>
-      </body>
 
-    </div>
+    <body className="App-body">
+      <audio autoPlay loop>
+        <source src="/audio/background_audio.wav" type="audio/wav" />
+        Your browser does not support the audio element.
+      </audio>
+      <h1> Izotope Games Character Generator</h1>
+      <div className="mid-div">
+        <a href="https://substack.com/@izotopegames?utm_source=user-menu">Link to Izotope Games Substack</a>
+        <br/>
+        <button>Generate Character</button>
+        <br/>
+        <button>Help</button>
+        <p>
+          This is where the generator button and help button will be.
+        </p>
+      </div>
+      <div className="bottom-div">
+        <p>
+          This is where generated characters will go in separate boxes.
+          Will need to implement a list or  table.
+        </p>
+      </div>
+    </body>
+    </html>
   );
 }
 
